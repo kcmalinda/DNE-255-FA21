8b6b202d22ce3eeb2986419da685ec7f15109cfe
KCMalinda <70920308+kcmalinda@users.noreply.github.com>
2021-11-18 16:27:47 -0500

    Completed searching system log events by keywords

---
+++ week08/homework/Search system log events by keyword.ps1
@@ -0,0 +1,18 @@
+﻿# Storyline: Review the Security Event Log
+
+# Directory to save files:
+
+$myDir = "C:\Users\alice.marni\Desktop\"
+
+# List all the available windows Event logs
+Get-EventLog -List
+# you could type: Get-EventLog Security 
+## -When run it shows what would show up when you are in event viewer (all the logs)
+
+# Create a prompt to allow user to select the log to view
+$readLog = Read-host -Prompt "Please select a log to review from the list above"
+$keyWord = Read-host -Prompt "Please type a keyword or a phrase to search on"
+
+# Print the results for the log
+
+Get-EventLog -LogName $readLog -Newest 40 | where {$_.Message -ilike "*$keyWord*"} | export-csv -NoTypeInformation -Path "$myDir\$readLog.csv"
